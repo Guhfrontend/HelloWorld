3ec45d093dbfb1eee0a961e8c4e8dcf4686bb8ed
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-07-01 15:37:12 -0300

    Refactor: -2 class colocada no controller

--- src/main/java/com/helloWorld/helloWorld/controller/HelloWorldController.java
+++ src/main/java/com/helloWorld/helloWorld/controller/HelloWorldController.java
@@ -5,12 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/helloworld")
+@RequestMapping("/api")
 public class HelloWorldController {
 
-    @GetMapping
+    @GetMapping("helloworld")
     public String helloWorld(){
         return "Hello World";
     }
 
+    @GetMapping("/bsm")
+    public String bsm() {
+        return "BSM's :\n" +
+                "Mentalidade: Orientação ao futuro, Responsabilidade pessoal, Mentalidade de Crescimento e " +
+                "Persistencia. \n\n" +
+                "Habilidade: Trabalho em equipe, Atenção aos detalhes, proatividade e comunicação";
+    }
+
+    @GetMapping("/calendary")
+    public String calendary() {
+        return "Objetivos da semana: Gestão Financeira, \n Eco Sistema Spring, \n Sessão com o Bem Estar \n e estudo de caso.";
+    }
 }
